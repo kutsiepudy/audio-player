492e829cbc9ba32e4be97fe37f8da00bb19d13ea
Kristin <kutsiepudy3841@gmail.com>
2026-04-20 10:19:27 -0700

    Fix typo in currentTimeEl assignment

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ audio.addEventListener("timeupdate", () => {
     let value = (audio.currentTime / audio.duration) * 100;
     seekSlider.value = value;
   }
-  cutrentTimeEl.textContent = formatTime(audio.currentTime);
+  currentTimeEl.textContent = formatTime(audio.currentTime);
 });
 
 audio.addEventListener("loadedmetadata", () => {
