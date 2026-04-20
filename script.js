d970cbc3e99c638738838b1168f950a353bf167d
Kristin <kutsiepudy3841@gmail.com>
2026-04-20 09:33:36 -0700

    Fix typo in cutrentTimeEl variable

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ audio.addEventListener("timeupdate", () => {
     let value = (audio.currentTime / audio.duration) * 100;
     seekSlider.value = value;
   }
-  cutrentTimeel.textContent = formatTime(audio.duration);
+  cutrentTimeEl.textContent = formatTime(audio.duration);
 });
 
 audio.addEventListener("loadedmetadata", () => {
